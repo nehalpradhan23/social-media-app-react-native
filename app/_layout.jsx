124ef9853e809096dad82a128f48a4c18b4dd8e2
Nehal pradhan <40754890+nehalpradhan23@users.noreply.github.com>
2024-08-27 05:35:10 +0530

    home screen, user context, login user

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,8 +1,41 @@
 import { View, Text } from "react-native";
-import React from "react";
-import { Stack } from "expo-router";
+import React, { useEffect } from "react";
+import { Stack, useRouter } from "expo-router";
+import { AuthProvider, useAuth } from "../context/AuthContext";
+import { supabase } from "../lib/supabase";
+import { getUserData } from "../services/userService";
 
 const _layout = () => {
+  return (
+    <AuthProvider>
+      <MainLayout />
+    </AuthProvider>
+  );
+};
+
+const MainLayout = () => {
+  const router = useRouter();
+  const { setAuth, setUserData } = useAuth();
+
+  useEffect(() => {
+    supabase.auth.onAuthStateChange((_event, session) => {
+      if (session) {
+        setAuth(session?.user);
+        updateUserData(session?.user);
+        router.replace("/home");
+      } else {
+        setAuth(null);
+        router.replace("/welcome");
+      }
+    });
+  }, []);
+
+  const updateUserData = async (user) => {
+    let res = await getUserData(user?.id);
+    if (res.success) {
+      setUserData(res.data);
+    }
+  };
   return <Stack screenOptions={{ headerShown: false }} />;
 };
 
