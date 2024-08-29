bd092d98a47d7cdbf69cca6af68454cf5f45f1e7
Nehal pradhan <40754890+nehalpradhan23@users.noreply.github.com>
2024-08-30 00:05:42 +0530

    update/edit profile, upload image

--- services/imageService.js
+++ services/imageService.js
@@ -1,7 +1,55 @@
+import * as FileSystem from "expo-file-system";
+import { decode } from "base64-arraybuffer";
+import { supabase } from "@/lib/supabase";
+import { supabaseUrl } from "@/constants";
+
 export const getUserImageSrc = (imagePath) => {
   if (imagePath) {
-    return { uri: imagePath };
+    return getSupabaseFileUrl(imagePath);
   } else {
     return require("../assets/images/defaultUser.png");
   }
+};
+
+export const getSupabaseFileUrl = (filePath) => {
+  if (filePath) {
+    return {
+      uri: `${supabaseUrl}/storage/v1/object/public/uploads/${filePath}`,
+    };
+  }
+  return null;
+};
+
+export const uploadFile = async (folderName, fileUri, isImage = true) => {
+  try {
+    let fileName = getFilePath(folderName, isImage);
+
+    const fileBase64 = await FileSystem.readAsStringAsync(fileUri, {
+      encoding: FileSystem.EncodingType.Base64,
+    });
+    let imageData = decode(fileBase64); //get  array buffer
+
+    // store in supabase
+    let { data, error } = await supabase.storage
+      .from("uploads")
+      .upload(fileName, imageData, {
+        cacheControl: "3600",
+        upsert: false,
+        contentType: isImage ? "image/*" : "video/*",
+      });
+
+    if (error) {
+      console.log("File upload error", error);
+      return { success: false, msg: "Image upload failed" };
+    }
+
+    return { success: true, data: data.path };
+  } catch (error) {
+    console.log("File upload error", error);
+    return { success: false, msg: "Image upload failed" };
+  }
+};
+
+export const getFilePath = (folderName, isImage) => {
+  return `/${folderName}/${new Date().getTime()}${isImage ? ".png" : ".mp4"}`;
 };
--- services/userService.js
+++ services/userService.js
@@ -20,3 +20,23 @@ export const getUserData = async (userId) => {
     return { success: false, msg: error.message };
   }
 };
+
+export const updateUser = async (userId, data) => {
+  try {
+    const { error } = await supabase
+      .from("users")
+      .update(data)
+      .eq("id", userId);
+
+    if (error) {
+      return { success: false, msg: error.message };
+    }
+    return {
+      success: true,
+      data,
+    };
+  } catch (error) {
+    console.log("user fetching data error", error);
+    return { success: false, msg: error.message };
+  }
+};
