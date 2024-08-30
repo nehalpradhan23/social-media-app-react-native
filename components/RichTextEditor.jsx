d597f5e25d764b2f54ae4e280b87196a14ca66d6
Nehal pradhan <40754890+nehalpradhan23@users.noreply.github.com>
2024-08-30 22:43:14 +0530

    create post - editor

---
+++ components/RichTextEditor.jsx
@@ -0,0 +1,86 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import {
+  actions,
+  RichEditor,
+  RichToolbar,
+} from "react-native-pell-rich-editor";
+import { theme } from "@/constants/theme";
+
+const RichTextEditor = ({ editorRef, onChange }) => {
+  // return (
+  //   <View>
+  //     <Text>editor</Text>
+  //   </View>
+  // );
+  return (
+    <View style={{ minHeight: 258 }}>
+      <RichToolbar
+        actions={[
+          actions.setStrikethrough,
+          actions.undo,
+          actions.removeFormat,
+          actions.setBold,
+          actions.setItalic,
+          actions.insertOrderedList,
+          actions.blockquote,
+          actions.alignLeft,
+          actions.alignCenter,
+          actions.alignRight,
+          actions.code,
+          actions.line,
+          actions.heading1,
+          actions.heading4,
+        ]}
+        iconMap={{
+          [actions.heading1]: ({ tintColor }) => (
+            <Text style={{ color: tintColor }}>H1</Text>
+          ),
+          [actions.heading4]: ({ tintColor }) => (
+            <Text style={{ color: tintColor }}>H4</Text>
+          ),
+        }}
+        style={styles.richBar}
+        flatContainerStyle={styles.flatStyle}
+        selectedIconTint={theme.colors.primaryDark}
+        editor={editorRef}
+        disabled={false}
+      />
+      <RichEditor
+        ref={editorRef}
+        containerStyle={styles.rich}
+        editorStyle={styles.contentStyle}
+        placeholder={"Enter text here"}
+        onChange={onChange}
+      />
+    </View>
+  );
+};
+
+export default RichTextEditor;
+
+const styles = StyleSheet.create({
+  richBar: {
+    borderTopRightRadius: theme.radius.xl,
+    borderTopLeftRadius: theme.radius.xl,
+    backgroundColor: theme.colors.gray,
+  },
+  flatStyle: {
+    paddingHorizontal: 8,
+    gap: 3,
+  },
+  rich: {
+    minHeight: 200,
+    flex: 1,
+    borderWidth: 1.5,
+    borderTopWidth: 0,
+    borderBottomLeftRadius: theme.radius.xl,
+    borderBottomRightRadius: theme.radius.xl,
+    borderColor: theme.colors.gray,
+    padding: 5,
+  },
+  contentStyle: {
+    color: theme.colors.textDark,
+    placeholderColor: "gray",
+  },
+});
